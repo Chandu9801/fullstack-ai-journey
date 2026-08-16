b2abe158649219d6ee0ab55022c95f1d6d1e39e3
Chandan Kumar <ck597436@gmail.com>
2026-08-16 09:53:43 +0530

    day 01: learn string data type

--- javascript/day-01.js
+++ javascript/day-01.js
@@ -2,15 +2,18 @@ const name = "Chandan";
 const age = 22;
 const college = "NSHM Knowledge Campus";
 const branch = "CSE";
-
-
-console.log("Name:", name);
-console.log("Age:", age);
-console.log("College:", college);
-console.log("Branch:", branch);
-
+const marks = 85.5;
+const cgpa = 8.5;
 console.log("Data Types:");
-
+console.log("Name:", name);
 console.log(typeof name);
+console.log("Age:", age);
+console.log(typeof age);
+console.log("College:", college);
 console.log(typeof college);
+console.log("Branch:", branch); 
 console.log(typeof branch);
+console.log("Marks:", marks);
+console.log(typeof marks);
+console.log("CGPA:", cgpa);
+console.log(typeof cgpa);
