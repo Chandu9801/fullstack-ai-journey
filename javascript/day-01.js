4f3a43f1f2590f2c38bef87724a31ea6d6e77c46
Chandan Kumar <ck597436@gmail.com>
2026-08-16 10:13:15 +0530

    day 01: learn string data type

--- javascript/day-01.js
+++ javascript/day-01.js
@@ -5,15 +5,31 @@ const branch = "CSE";
 const marks = 85.5;
 const cgpa = 8.5;
 console.log("Data Types:");
+
 console.log("Name:", name);
 console.log(typeof name);
+
 console.log("Age:", age);
 console.log(typeof age);
+
 console.log("College:", college);
 console.log(typeof college);
+
 console.log("Branch:", branch); 
 console.log(typeof branch);
+
 console.log("Marks:", marks);
 console.log(typeof marks);
+
 console.log("CGPA:", cgpa);
 console.log(typeof cgpa);
+
+const isStudent = true; 
+console.log("isstudent:-",isStudent);
+console.log(typeof isStudent);
+const isAdmin = false;
+console.log("isAdmin:-",isAdmin);
+console.log(typeof isAdmin);
+const isLoggedIn = true;
+console.log("isLoggedIn:-",isLoggedIn);
+console.log(typeof isLoggedIn);
