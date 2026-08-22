92e9552577a7c32b1ef4a556d20e5d773835beca
CHANDAN KUMBHAKAR <ck597436@gmail.com>
2026-08-22 14:07:44 +0530

    day 01: loop

--- javascript/loop.js
+++ javascript/loop.js
@@ -1,3 +1,4 @@
+// print the no from 1 to 50 with even and odd numbers wise 
 let i = 1;
 for (i = 1; i <= 50; i++) {
     if (i % 2 === 0) {
@@ -6,3 +7,12 @@ for (i = 1; i <= 50; i++) {
         console.log("Odd number: " , i);
     }   
 }
+
+//Question: Find the sum of all even numbers from 1 to 100.
+let sum = 0;   
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0) {
+        sum += i;
+    }
+}  
+console.log("Sum of even numbers from 1 to 100: " , sum);
